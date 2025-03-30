95e83d8c45afcbface6ab6269518e34ff25d2a64
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-30 15:19:04 +0200

    use env

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -34,7 +36,7 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -44,7 +46,7 @@ app.use('/**', (req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
     .catch(next);
 });
