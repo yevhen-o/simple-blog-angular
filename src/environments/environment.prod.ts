f90b2cd2a3795b1fe261acefafb1f2194910560d
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-30 16:16:45 +0200

    Try fix env

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: process.env['VITE_SUPABASE_URL'], // Empty for now build-env will place here data
-  supabaseKey: process.env['VITE_SUPABASE_KEY'], // Empty for now build-env will place here data
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: process.env['VITE_SUPABASE_URL'], // Empty for now build-env will place here data
-  supabaseKey: process.env['VITE_SUPABASE_KEY'], // Empty for now build-env will place here data
+  supabaseUrl: process.env['VITE_SUPABASE_URL'],
+  supabaseKey: process.env['VITE_SUPABASE_KEY'],
 };
