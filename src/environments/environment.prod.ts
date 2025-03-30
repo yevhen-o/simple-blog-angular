9a95124a2ec8a556232e0d025797a3b4b6363d6d
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-30 15:21:10 +0200

    Add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: process.env['VITE_SUPABASE_URL'], // Empty for now build-env will place here data
+  supabaseKey: process.env['VITE_SUPABASE_KEY'], // Empty for now build-env will place here data
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: process.env['VITE_SUPABASE_URL'], // Empty for now build-env will place here data
+  supabaseKey: process.env['VITE_SUPABASE_KEY'], // Empty for now build-env will place here data
+};
