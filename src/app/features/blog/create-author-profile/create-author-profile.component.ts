93eee6e65a2c99ff4e55119125c5958bd632d86c
Yevhen Oliynyk <i.yevhen.oliynyk@gmail.com>
2025-03-30 15:39:09 +0200

    Remove unused code

--- src/app/features/blog/create-author-profile/create-author-profile.component.ts
+++ src/app/features/blog/create-author-profile/create-author-profile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   FormGroup,
   ReactiveFormsModule,
